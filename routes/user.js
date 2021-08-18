878250b87cf57cd87a831167d1b1ffb93eede780
parktaewoo <gesazumoo@gmail.com>
2021-08-18 13:21:30 +0900

    summary api 변경.

--- routes/user.js
+++ routes/user.js
@@ -63,33 +63,65 @@ userRouter.get('/recentGameSummary/:id', async (req, res, next) => {
             return game
         }))
 
-        let win = 0
-        let lose = 0
-        let kills = 0
-        let deaths = 0
-        let assists = 0
-
+        const summary = {
+            totalWin:0,
+            totalLose:0,
+            totalKills: 0,
+            totalDeaths: 0,
+            totalAssists: 0,
+            gameList: [
+                {
+                    win: true,
+                    kill:0,
+                    deaths: 0,
+                    asi: 0,
+                    champion: 0,
+                    teamKill: 0,
+                    line: ''
+                }
+            ]   
+        }
+        
+        let totalKills = 0
         result.forEach(game => {
             const userIndex = game.detail.participantIdentities.findIndex(pid => {
                 return pid.player.accountId == req.params.id
             })
 
             const participants = game.detail.participants[userIndex]
-            if(participants.stats.win) win++
-            else lose++
+            if(participants.stats.win) summary.win++
+            else summary.totalLose++
 
-            kills = kills + participants.stats.kills
-            deaths = deaths + participants.stats.deaths
-            assists = assists + participants.stats.assists
+            summary.totalKills = summary.totalKills + participants.stats.kills
+            summary.totalDeaths = summary.totalDeaths + participants.stats.deaths
+            summary.totalAssists = summary.totalAssists + participants.stats.assists
+
+            summary.gameList.push({
+                win: participants.stats.win,
+                kills: participants.stats.kills,
+                deaths: participants.stats.deaths,
+                assists: participants.stats.assists,
+                championId: participants.championId,
+                line: ''
+            })
+            
+            const gameKill = game.detail.participants
+            .filter(prt => {
+                return participants.teamId == prt.teamId
+            })
+            .map(prt => {
+                return prt.stats.kills
+            })
+            .reduce((arr, cntKill) => {
+                return arr + cntKill
+            })
+
+            totalKills = totalKills + gameKill
         })
 
         const summaryData = {
-            win,
-            lose,
-            winRate: win/avrCount*100,
-            killAvg:kills/avrCount,
-            deathAvg:deaths/avrCount,
-            assistAvg:assists/avrCount
+            teamTotalKills : totalKills,
+            ...summary
         }
         return res.json(summaryData)
     }
