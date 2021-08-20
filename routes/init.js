11d5454ab9b7cd7c67b6ba909a01025b9c32bbb3
teawoo <geaszumoo@gmailcom>
2021-08-20 13:52:04 +0900

    test

--- routes/init.js
+++ routes/init.js
@@ -5,19 +5,20 @@ import {fetchRotationChamps} from '../riotApi.js'
 const initRouter = express.Router()
 
 initRouter.get('/rotation', async (req, res, next) => {
-    try{
-        const post = new PostModel({
-            title: '123',
-            author: 'author',
-            body: 'bodydydydyy',
-        })
-        await post.save()
-        const {data} = await fetchRotationChamps()
-        return res.json(data)
-    }
-    catch(err) {
-        next(err)
-    }
+    // try{
+    //     const post = new PostModel({
+    //         title: '123',
+    //         author: 'author',
+    //         body: 'bodydydydyy',
+    //     })
+    //     await post.save()
+    //     const {data} = await fetchRotationChamps()
+    //     return res.json(data)
+    // }
+    // catch(err) {
+    //     next(err)
+    // }
+    return res.json({init:true})
 })
 
 export default initRouter 
