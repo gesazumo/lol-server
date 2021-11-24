db0b748dd26aa48b84bee0da537d4bae82934735
DESKTOP-74CHEV1\gesaz <gesazumo>
2021-11-24 13:25:27 +0900

    이전글 다음글

--- routes/board.js
+++ routes/board.js
@@ -34,11 +34,37 @@ boardRouter.get('/:id', async (req, res, next) => {
     try {
         const {id} = req.params
         const board = await BoardModel.findById(id)
-        return res.json(board)
+        if(!board) return res.json(board)
+        else return res.status(404).send('board not found')
     }
     catch(err) {
         next(err)
     }
 })
+
+boardRouter.get('/:id/previous', async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const board = await BoardModel.findOne({_id: {$lt: id}}).sort({_id: -1})
+        if(board) return res.json(board)
+        else return res.status(404).send('previous board not found')
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+boardRouter.get('/:id/next', async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const board = await BoardModel.findOne({_id: {$gt: id}}).sort({_id: 1})
+        if(board) return res.json(board)
+        else return res.status(404).send('next board not found')
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
  
 export default boardRouter 
